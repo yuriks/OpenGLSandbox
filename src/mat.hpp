ae5f7d6f7958d8ea876771f62d486e9daf513131
Yuri K. Schlesner <yuriks@yuriks.net>
2012-11-24 17:18:36 -0500

    Added to and fixed matrix class and added tests.

--- src/mat.hpp
+++ src/mat.hpp
@@ -12,26 +12,40 @@ struct mat {
 	// Rows
 	vec<C> data[R];
 
+	// Basic element acessors
 	inline float& operator ()(unsigned int i, unsigned int j) {
-		ASSERT(i < R && j < C);
+		assert(i < R && j < C);
 		return data[i][j];
 	}
 
-	inline const float& operator [](unsigned int i) const {
-		ASSERT(i < R && j < C);
+	inline const float& operator ()(unsigned int i, unsigned int j) const {
+		assert(i < R && j < C);
 		return data[i][j];
 	}
 
+	inline bool operator ==(const mat<R,C>& o) const {
+		bool eq = true;
+		for (unsigned int i = 0; i < R; ++i) {
+			eq = eq && data[i] == o.data[i];
+		}
+		return eq;
+	}
+
+	inline bool operator !=(const mat<R,C>& o) const {
+		return !(*this == o);
+	}
+
+	// Component-wise matrix arithmetic
 #define DEFINE_OP(op) \
-	inline mat<R,C>& operator op=(const mat<R,C>& o) { \
+	inline mat<R,C>& operator op##=(const mat<R,C>& o) { \
 		for (unsigned int i = 0; i < R; ++i) { \
-			data[i] op= o.data[i]; \
+			data[i] op##= o.data[i]; \
 		} \
 		return *this; \
 	} \
-	inline mat<R,C> operator op(const mat<R,C>& o) { \
-		mat<R,N> r = *this; \
-		return r op= o; \
+	inline mat<R,C> operator op(const mat<R,C>& o) const { \
+		mat<R,C> r = *this; \
+		return r op##= o; \
 	}
 
 	DEFINE_OP(+)
@@ -39,35 +53,54 @@ struct mat {
 
 #undef DEFINE_OP
 
-#define DEFINE_SCALAR_OP(op) \
-	inline mat<R,C>& operator op=(const float a) { \
-		for (unsigned int i = 0; i < N; ++i) { \
-			data[i] op= a; \
-		} \
-		return *this; \
-	} \
-	inline mat<R,C> operator op(const float a) { \
-		mat<R,N> r = *this; \
-		return r op= a; \
+	// Scalar multiplication
+	inline mat<R,C>& operator *=(const float a) {
+		for (unsigned int i = 0; i < R; ++i) {
+			data[i] *= a;
+		}
+		return *this;
 	}
 
-	DEFINE_SCALAR_OP(*)
-	DEFINE_SCALAR_OP(/)
-
-#undef DEFINE_SCALAR_OP
-
-	inline mat<R,C> operator -() {
+	inline mat<R,C> operator *(const float a) const {
 		mat<R,C> r = *this;
-		for (unsigned int i = 0; i < N; ++i) {
+		return r *= a;
+	}
+
+	inline mat<R,C> operator -() const {
+		mat<R,C> r = *this;
+		for (unsigned int i = 0; i < R; ++i) {
 			r.data[i] = -r.data[i];
 		}
 		return r;
 	}
 };
 
+template<unsigned int R, unsigned int C>
+inline mat<R,C> operator *(const float a, const mat<R,C>& m) {
+	return m * a;
+}
+
+// Convenience typedefs for the most common kinds of matrices.
 typedef mat<2> mat2;
 typedef mat<3> mat3;
 typedef mat<4> mat4;
+
+// Convenient constants for matrices.
+static const mat2 mat2_identity = {{
+	{1.0f, 0.0f},
+	{0.0f, 1.0f}
+}};
+static const mat3 mat3_identity = {{
+	{1.0f, 0.0f, 0.0f},
+	{0.0f, 1.0f, 0.0f},
+	{0.0f, 0.0f, 1.0f}
+}};
+static const mat4 mat4_identity = {{
+	{1.0f, 0.0f, 0.0f, 0.0f},
+	{0.0f, 1.0f, 0.0f, 0.0f},
+	{0.0f, 0.0f, 1.0f, 0.0f},
+	{0.0f, 0.0f, 0.0f, 1.0f}
+}};
 
 template<unsigned int M, unsigned int N, unsigned int P>
 mat<M,P> operator *(const mat<M,N>& a, const mat<N,P>& b) {
@@ -84,6 +117,20 @@ mat<M,P> operator *(const mat<M,N>& a, const mat<N,P>& b) {
 	}
 
 	return r;
+}
+
+// Prints matrix m to stream.
+template<unsigned int R, unsigned int C>
+std::ostream& operator <<(std::ostream& s, const mat<R,C>& m) {
+	for (unsigned int i = 0; i < R; ++i) {
+		s << '|';
+		for (unsigned int j = 0; j < C-1; ++j) {
+			s << m(i, j) << " ";
+		}
+		s << m(i, C-1) << "|\n";
+	}
+
+	return s;
 }
 
 }
---
+++ test/mat_test.cpp
@@ -0,0 +1,81 @@
+#include <boost/test/unit_test.hpp>
+
+#include "mat.hpp"
+
+BOOST_AUTO_TEST_CASE(MatrixClassTest) {
+	using namespace hw::math;
+
+	const mat2 a = {{
+		{1.0f, 2.0f},
+		{3.0f, 4.0f}
+	}};
+
+	const mat2 b = {{
+		{10.0f, 5.0f},
+		{ 2.0f, 2.0f}
+	}};
+
+	// Test basic acessors.
+	BOOST_CHECK_EQUAL(a(0,0), 1.0f);
+	BOOST_CHECK_EQUAL(a(0,1), 2.0f);
+	BOOST_CHECK_EQUAL(a(1,0), 3.0f);
+	BOOST_CHECK_EQUAL(a(1,1), 4.0f);
+
+	// Test equality, assignment and copying.
+	BOOST_CHECK(a == mat2(a));
+	BOOST_CHECK(a != b);
+	{
+		mat2 c = mat2_identity;
+		c = a;
+		BOOST_CHECK(c != mat2_identity);
+		BOOST_CHECK_EQUAL(c, a);
+	}
+
+	// Test component-wise arithmetic
+	const mat2 a_plus_b = {{
+		{11.0f, 7.0f},
+		{ 5.0f, 6.0f}
+	}};
+
+	BOOST_CHECK_EQUAL(a + b, a_plus_b);
+	BOOST_CHECK_EQUAL(a_plus_b - b, a);
+
+	// Test scaling
+	{
+		const mat2 b2 = {{
+			{20.0f, 10.0f},
+			{ 4.0f,  4.0f}
+		}};
+
+		BOOST_CHECK_EQUAL(b * 2.0f, b2);
+		BOOST_CHECK_EQUAL(2.0f * b, b2);
+	}
+
+	// Test component-wise negation
+	{
+		const mat2 minus_a = {{
+			{-1.0f, -2.0f},
+			{-3.0f, -4.0f}
+		}};
+
+		BOOST_CHECK_EQUAL(-a, minus_a);
+	}
+}
+
+BOOST_AUTO_TEST_CASE(MatrixFuncsTest) {
+	using namespace hw::math;
+
+	const mat2 a = {{
+		{1.0f, 2.0f},
+		{3.0f, 4.0f}
+	}};
+
+	const mat2 a_times_a = {{
+		{ 7.0f, 10.0f},
+		{15.0f, 22.0f}
+	}};
+
+	// Test matrix multiplication
+	BOOST_CHECK_EQUAL(a * mat2_identity, a);
+	BOOST_CHECK_EQUAL(a * a, a_times_a);
+}
