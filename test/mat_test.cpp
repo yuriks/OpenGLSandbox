1371689b4cd531005bb9ef8acd529b66024f3230
Yuri K. Schlesner <yuriks@yuriks.net>
2012-11-29 13:55:54 -0600

    Added comment

--- test/mat_test.cpp
+++ test/mat_test.cpp
@@ -98,6 +98,7 @@ BOOST_AUTO_TEST_CASE(MatrixFuncsTest) {
 	BOOST_CHECK_EQUAL(a * a, a_times_a);
 	BOOST_CHECK_EQUAL(column_matrix * (2.0f * row_matrix), outer_product);
 
+	// Test matrix->vector conversion
 	const vec3 vector = {{1.0f, 2.0f, 3.0f}};
 
 	BOOST_CHECK_EQUAL(vector, vec_from_mat(column_matrix));
